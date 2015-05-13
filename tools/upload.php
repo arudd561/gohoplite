f4db1d89310a740349acf73a8993f2de4855df51
Brian Ballard <brian@gohoplite.com>
2015-05-12 20:19:19 -0600

    Added file upload

---
+++ tools/upload.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>File Uploader</title>
+	</head>
+	<body>
+	
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+	<?php
+		if(count($_FILES) > 0) {
+			$message = "";
+			$target_path = "uploads/";
+			
+			echo (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path))
+				? ("The file ".  basename( $_FILES['uploadedfile']['name']). " has been uploaded")
+				: "There was an error uploading the file, please try again!"
+			?>
+			Select another file to upload:
+			<?php
+		}
+		else {
+			?>
+			Select a file to upload:
+			<?php
+		}
+	?>
+	    <input type="file" name="fileToUpload" id="fileToUpload">
+	    <input type="submit" value="Upload" name="submit">
+	</form>
+	
+	</body>
+</html> 
