2528b8a6a9ab37e3847875c96085b5ff327f8e3a
Brian Ballard <brian.ballard@gohoplite.com>
2016-07-13 22:22:00 -0600

    Updated css to support hovers on signup options.  Added signup pages

---
+++ signup/signup.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html>
+        <head>
+                <title>Hoplite, Signup</title>
+                <link rel="stylesheet" type="text/css" href="../style/font-icon.css" />
+                <link rel="stylesheet" type="text/css" href="../style/base.css" />
+                <link rel="stylesheet" type="text/css" href="../style/index.css" />
+        </head>
+        <body>
+                <script type="text/javascript">
+                </script>
+                
+                <style type="text/css">
+                        
+                </style>
+
+                <div id="headerDiv" class="layout-div">
+                        <div>
+                                <div id="logoDiv">&nbsp;</div>
+                                <div id="searchDiv">
+                                        <input type="text" id="searchTextbox" name="searchTextbox" />
+                                        <label for="searchTextbox">&#x55;</label>
+                                </div>
+                        </div>
+                </div>
+
+                <div id="menuDiv" class="layout-div">
+                </div>
+                
+                <div id="bannerDiv">
+                    <div>
+                        Choose your configuration
+                    </div>
+                </div>
+                
+                <div id="contentDiv" class="layout-div">
+                        <div>
+                                <div id="lobBannerDiv">
+                                        <div id="productDevelopmentBanner" class="option-banner">
+                                            <a href="cloud.php" style="width: 100%; height: 100%">
+                                                <div class="banner-header">
+                                                        <h2>
+                                                                <div class="elegant banner-icon">&#xe002;</div>
+                                                                Cloud
+                                                        </h2>
+                                                </div>
+                                                <div class="banner-content">
+                                                        <p> 
+                                                            This is a private instance on a shared server. 
+                                                        </p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        
+                                        <div id="automationBanner" class="option-banner">
+                                            <a href="vps.php" style="width: 100%; height: 100%">
+                                                <div class="banner-header">
+                                                        <h2>
+                                                                <div class="elegant banner-icon">&#xe0ea;</div>
+                                                                Virtual Private Server
+                                                        </h2>
+                                                </div>
+                                                <div class="banner-content">
+                                                        <p>
+                                                            Host your project manager on a private server.
+                                                            This option enables you to host a web page, and git or svn repositories.  
+                                                        </p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        
+                                        <div id="softwareBanner" class="option-banner">
+                                            <a href="onpremesis.php" style="width: 100%; height: 100%">
+                                                <div class="banner-header">
+                                                        <h2>
+                                                                <div class="elegant banner-icon">&#xe0ef;</div>
+                                                                On Premesis
+                                                        </h2>
+                                                </div>
+                                                <div class="banner-content">
+                                                        <p>
+                                                            Improve accessibilty by hosting on premesis.
+                                                            You may lease or purchase a preconfigured server.
+                                                            We have virtual machine images available for VM Ware, Xen, KVM, and Virtual Box.  
+                                                        </p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+
+                <div id="footerDiv" class="layout-div">
+                        <div>
+                                footer
+                        </div>
+                </div>
+        </body>
+</html>
---
+++ signup/vps.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html>
+        <head>
+                <title>Hoplite, Signup</title>
+                <link rel="stylesheet" type="text/css" href="../style/font-icon.css" />
+                <link rel="stylesheet" type="text/css" href="../style/base.css" />
+                <link rel="stylesheet" type="text/css" href="../style/index.css" />
+        </head>
+        <body>
+                <script type="text/javascript">
+                </script>
+                
+                <style type="text/css">
+                        
+                </style>
+
+                <div id="headerDiv" class="layout-div">
+                        <div>
+                                <div id="logoDiv">&nbsp;</div>
+                                <div id="searchDiv">
+                                        <input type="text" id="searchTextbox" name="searchTextbox" />
+                                        <label for="searchTextbox">&#x55;</label>
+                                </div>
+                        </div>
+                </div>
+
+                <div id="menuDiv" class="layout-div">
+                </div>
+                
+                <div id="bannerDiv">
+                    <div>
+                        Choose your configuration
+                    </div>
+                </div>
+                
+                <div id="contentDiv" class="layout-div">
+                        <div>
+                                <div id="lobBannerDiv">
+                                        <div id="productDevelopmentBanner" class="option-banner">
+                                            <a href="#" style="width: 100%; height: 100%">
+                                                <div class="banner-header">
+                                                        <h2>
+                                                                <div class="elegant banner-icon">&#xe0df;</div>
+                                                                Premium
+                                                        </h2>
+                                                </div>
+                                                <div class="banner-content">
+                                                        <p> 
+                                                            60GB storage with support for API 
+                                                        </p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        
+                                        <div id="automationBanner" class="option-banner">
+                                                <div class="banner-header">
+                                                        <h2>
+                                                                <div class="elegant banner-icon">&#xe107;</div>
+                                                                Plus
+                                                        </h2>
+                                                </div>
+                                                <div class="banner-content">
+                                                        <p>
+                                                            60GB storage  
+                                                        </p>
+                                                </div>
+                                        </div>
+                                        
+                                        <div id="softwareBanner" class="option-banner">
+                                                <div class="banner-header">
+                                                        <h2>
+                                                                <div class="elegant banner-icon">&#xe07a;</div>
+                                                                Standard
+                                                        </h2>
+                                                </div>
+                                                <div class="banner-content">
+                                                        <p>
+                                                            30GB storage independently managed.
+                                                        </p>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+
+                <div id="footerDiv" class="layout-div">
+                        <div>
+                                footer
+                        </div>
+                </div>
+        </body>
+</html>
