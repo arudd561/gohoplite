2e7c4296208660131a87c417b0f200feb3cbae5e
Brian Ballard <brian@tappcom.com>
2016-08-25 17:31:14 -0600

    Added new media to the site

--- index.php
+++ index.php
@@ -1,161 +1,221 @@
 <!doctype html>
 <html>
-        <head>
-                <title>Hoplite</title>
-                <link rel="stylesheet" type="text/css" href="style/font-icon.css" />
-                <link rel="stylesheet" type="text/css" href="style/base.css" />
-                <link rel="stylesheet" type="text/css" href="style/index.css" />
-        </head>
-        <body>
-                <script type="text/javascript">
-                </script>
-                
-                <style type="text/css">
-                        
-                </style>
+    <head>
+        <title>Hoplite</title>
+        <link rel="icon" type="image/png" href="favicon.png?v1" />
+        <link rel="stylesheet" type="text/css" href="style/font-icon.css" />
+        <link rel="stylesheet" type="text/css" href="style/base.css" />
+        <link rel="stylesheet" type="text/css" href="style/index.css" />
+    </head>
+    <body>
+        <script type="text/javascript">
+        </script>
 
-                <div id="headerDiv" class="layout-div">
-                        <div>
-                                <div id="logoDiv">&nbsp;</div>
-                                <div id="searchDiv">
-                                        <input type="text" id="searchTextbox" name="searchTextbox" />
-                                        <label for="searchTextbox">&#x55;</label>
-                                </div>
+        <style type="text/css">
+
+        </style>
+
+        <div id="headerDiv" class="layout-div">
+            <div>
+                <div id="logoDiv">&nbsp;</div>
+                <div id="searchDiv">
+                    <input type="text" id="searchTextbox" name="searchTextbox" />
+                    <label for="searchTextbox">&#x55;</label>
+                </div>
+            </div>
+        </div>
+
+        <div id="menuDiv" class="layout-div">
+        </div>
+
+        <div id="bannerDiv">
+            <div id="bannerContent">
+            </div>
+            <p>
+            Great thinking deserves great tools. Intelligent thinking in action. A comprehensive tool set to help manage your business.
+            </p>
+        </div>
+
+        <div id="contentDiv" class="layout-div">
+            <div>
+                <div class="section">
+                    <div>
+                        <h2>Project Management</h2>
+                        <div class="banner-icon">
+                            <img src="images/team_collab_05.jpg" width="250" />
                         </div>
-                </div>
-
-                <div id="menuDiv" class="layout-div">
-                </div>
-                
-                <div id="bannerDiv">
-                    <div id="bannerContent">
+                        <div class="section-content">
+                            1234
+                        </div>
                     </div>
-                    <p>
-                        Great thinking deserves great tools. Intelligent thinking in action. A comprehensive tool set to help manage your business.
-                    </p>
                 </div>
                 
-Projects
-Web Publishing
-Customers
-Online Sales
-Telecommunications
-                <div id="contentDiv" class="layout-div">
-                        <div>
-                                <div id="lobBannerDiv">
-                                        <div id="productDevelopmentBanner" class="lob-banner">
-                                                <div class="banner-header">
-                                                        <h2>
-                                                                <div class="elegant banner-icon">&#xe0df;</div>
-                                                                Project Management
-                                                        </h2>
-                                                </div>
-                                                <div class="banner-content">
-                                                        <ul>
-                                                                <li>Project &amp; Tasks
-                                                                        <ul>
-                                                                                <li>Concept Definition</li>
-                                                                                <li>Design Cycle</li>
-                                                                                <li>Proof of Concept</li>
-                                                                        </ul>
-                                                                </li>
-                                                                
-                                                                <li>Wiki Documentation
-                                                                        <ul>
-                                                                                <li>Plan</li>
-                                                                                <li>Build</li>
-                                                                                <li>Test</li>
-                                                                        </ul>
-                                                                </li>
-                                                                
-                                                                <li>Document Sharing
-                                                                        <ul>
-                                                                                <li>Consumer Trials</li>
-                                                                                <li>Market Preparations
-                                                                                        <ul>
-                                                                                                <li>Legal</li>
-                                                                                                <li>Marketing Consultation</li>
-                                                                                        </ul>
-                                                                                </li>
-                                                                                <li>Release</li>
-                                                                        </ul>
-                                                                </li>
-                                                                
-                                                                <li>News
-                                                                        <ul>
-                                                                                <li>Consumer Trials</li>
-                                                                                <li>Market Preparations
-                                                                                        <ul>
-                                                                                                <li>Legal</li>
-                                                                                                <li>Marketing Consultation</li>
-                                                                                        </ul>
-                                                                                </li>
-                                                                                <li>Release</li>
-                                                                        </ul>
-                                                                </li>
-                                                        </ul>
-                                                </div>
-                                        </div>
-                                        
-                                        <div id="automationBanner" class="lob-banner">
-                                                <div class="banner-header">
-                                                        <h2>
-                                                                <div class="elegant banner-icon">&#xe107;</div>
-                                                                Customer Relations
-                                                        </h2>
-                                                </div>
-                                                <div class="banner-content">
-                                                        <ul>
-                                                                <li>3 Axis Table
-                                                                  <ul>
-                                                                        <li>Milling</li>
-                                                                        <li>Plasma Cutting</li>
-                                                                        <li>Extrusion Printing</li>
-                                                                  </ul>
-                                                                </li>
-                                                                <li>Lathe
-                                                                  <ul>
-                                                                        <li>3 Axis</li>
-                                                                        <li>5 Axis</li>
-                                                                  </ul>
-                                                                </li>
-                                                                <li>Pick and Place</li>
-                                                                <li>Support</li>
-                                                        </ul>
-                                                </div>
-                                        </div>
-                                        
-                                        <div id="softwareBanner" class="lob-banner">
-                                                <div class="banner-header">
-                                                        <h2>
-                                                                <div class="elegant banner-icon">&#xe07a;</div>
-                                                                E-Commerce
-                                                        </h2>
-                                                </div>
-                                                <div class="banner-content">
-                                                        <ul>
-                                                                <li>Production</li>
-                                                                <li>Projects</li>
-                                                                <li>Customer Relations</li>
-                                                                <li>Point of Presence
-                                                                  <ul>
-                                                                        <li>Tapp</li>
-                                                                        <li>Web</li>
-                                                                        <li>Email</li>
-                                                                        <li>Phone and Text</li>
-                                                                  </ul>
-                                                                </li>
-                                                        </ul>
-                                                </div>
-                                        </div>
-                                </div>
-                        </div>
+                <div class="section">
+                    <div class="section-header">
+                        <h2>
+                            <span>Web Publishing</span>
+                            <div class="elegant banner-icon">&#xe0df;</div>
+                        </h2>
+                    </div>
+                    <div class="section-content">
+                        1234
+                    </div>
+                </div>
+                
+                <div class="section">
+                    <div class="section-header">
+                        <h2>
+                            <span>Customer Relationships</span>
+                            <div class="elegant banner-icon">&#xe0df;</div>
+                        </h2>
+                    </div>
+                    <div class="section-content">
+                        1234
+                    </div>
+                </div>
+                
+                <div class="section">
+                    <div class="section-header">
+                        <h2>
+                            <span>Online Sales</span>
+                            <div class="elegant banner-icon">&#xe0df;</div>
+                        </h2>
+                    </div>
+                    <div class="section-content">
+                        1234
+                    </div>
+                </div>
+                
+                <div class="section">
+                    <div class="section-header">
+                        <h2>
+                            <span>Voice and Text Communication</span>
+                            <div class="elegant banner-icon">&#xe0df;</div>
+                        </h2>
+                    </div>
+                    <div class="section-content">
+                        1234
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div>
+                <div id="lobBannerDiv">
+                <div id="productDevelopmentBanner" class="lob-banner">
+                <div class="banner-header">
+                <h2>
+                <div class="elegant banner-icon">&#xe0df;</div>
+                Project Management
+                </h2>
+                </div>
+                <div class="banner-content">
+                <ul>
+                <li>Project &amp; Tasks
+                <ul>
+                <li>Concept Definition</li>
+                <li>Design Cycle</li>
+                <li>Proof of Concept</li>
+                </ul>
+                </li>
+
+                <li>Wiki Documentation
+                <ul>
+                <li>Plan</li>
+                <li>Build</li>
+                <li>Test</li>
+                </ul>
+                </li>
+
+                <li>Document Sharing
+                <ul>
+                <li>Consumer Trials</li>
+                <li>Market Preparations
+                <ul>
+                <li>Legal</li>
+                <li>Marketing Consultation</li>
+                </ul>
+                </li>
+                <li>Release</li>
+                </ul>
+                </li>
+
+                <li>News
+                <ul>
+                <li>Consumer Trials</li>
+                <li>Market Preparations
+                <ul>
+                <li>Legal</li>
+                <li>Marketing Consultation</li>
+                </ul>
+                </li>
+                <li>Release</li>
+                </ul>
+                </li>
+                </ul>
+                </div>
                 </div>
 
-                <div id="footerDiv" class="layout-div">
-                        <div>
-                                footer
-                        </div>
+                <div id="automationBanner" class="lob-banner">
+                    <div class="banner-header">
+                        <h2>
+                            <div class="elegant banner-icon">&#xe107;</div>
+                            Customer Relations
+                        </h2>
+                    </div>
+                    <div class="banner-content">
+                        <ul>
+                            <li>3 Axis Table
+                                <ul>
+                                    <li>Milling</li>
+                                    <li>Plasma Cutting</li>
+                                <li>Extrusion Printing</li>
+                            </ul>
+                        </li>
+                        <li>Lathe
+                            <ul>
+                                <li>3 Axis</li>
+                                <li>5 Axis</li>
+                            </ul>
+                        </li>
+                        <li>Pick and Place</li>
+                        <li>Support</li>
+                    </ul>
                 </div>
-        </body>
+            </div>
+
+            <div id="softwareBanner" class="lob-banner">
+                <div class="banner-header">
+                    <h2>
+                        <div class="elegant banner-icon">&#xe07a;</div>
+                        E-Commerce
+                    </h2>
+                </div>
+                <div class="banner-content">
+                    <ul>
+                        <li>Production</li>
+                        <li>Projects</li>
+                        <li>Customer Relations</li>
+                        <li>Point of Presence
+                            <ul>
+                                <li>Tapp</li>
+                                <li>Web</li>
+                                <li>Email</li>
+                                <li>Phone and Text</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+
+        <div id="footerDiv" class="layout-div">
+            <div>
+                <div>(NPA) NXX - XXXX</div>
+                <div>support@gohoplite.com</div>
+            </div>
+        </div>
+    </body>
 </html>
